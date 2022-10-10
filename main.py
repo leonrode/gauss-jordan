3c8a0c4fca4ebd6196c40a3e452151dc518d36f0
Leon Rode <leon.rode13@gmail.com>
2022-10-09 21:59:58 -0400

    print_matrix, comments

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-matrix = [[1, 1, 2, 8], [-1, -2, 3, 1], [3, -7, 4, 10]]
+matrix = [[1, 2, -3, 4, 12], [2, 2, -2, 3, 10], [0, 1, 1, 0, -1], [1, -1, 1, -2, -4]]
 
 def multiply(row, value):
   return list(map(lambda x: value * x, row))
 
 def add(row1, row2):
   return [a + b for a, b in zip(row1, row2)]
+
+def print_matrix(matrix):
+  print("\n".join(["   ".join([str(number) for number in row]) for row in matrix]))
+  print("----------------")
 
 for i, row in enumerate(matrix):
   inverse = 1 / row[i]
@@ -13,6 +17,5 @@ for i, row in enumerate(matrix):
   for j, other_row in enumerate(matrix):
     if j != i:
       value = other_row[i]
-      matrix[j] = add(other_row, multiply(matrix[i], -value))
-
-print(matrix)
+      matrix[j] = add(other_row, multiply(matrix[i], -value)) # add(other_row, row multiplied by additive inverse) to make 0
+    print_matrix(matrix)
